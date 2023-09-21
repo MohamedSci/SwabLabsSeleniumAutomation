c57754404187814291e5953f8e2712d54188bbcb
MohamedSci <=>
2023-09-21 17:18:29 +0300

    First Test

--- src/test/java/com/featheredteam/LogIn/AppTest.java
+++ src/test/java/com/featheredteam/LogIn/AppTest.java
@@ -1,7 +1,5 @@
 package com.featheredteam.LogIn;
 
-import static org.junit.Assert.assertTrue;
-
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
@@ -10,11 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -23,9 +17,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
  * Unit test for simple App.
  */
 public class AppTest {
-    public static ChromeDriver driver;
+
+    /* there are 2 tests with the same Logic, but 
+                the first is failed has @BeforeTest @AfterTest @Test annotations 
+                  with org.openqa.selenium.NoSuchSessionException: invalid session id.
+                  The Second is passed and has @Test annotation only.
+                  */
+    public static WebDriver driver;
     
-@BeforeSuite
+@BeforeTest
 public void setUps(){
         System.out.println("Driver is started");
     WebDriverManager.chromedriver().setup();
@@ -39,7 +39,6 @@ public void setUps(){
    
     @Test
     public void shouldAnswerWithTrue(){
-       
             driver.get("https://www.saucedemo.com/");
             WebElement title = driver.findElement(By.xpath("//div[contains(text(),'Swag Labs')]"));
               Assert.assertEquals(title.getText(), "Swag Labs");
@@ -59,14 +58,10 @@ public void setUps(){
     driver.manage().window().maximize();
     driver.manage().deleteAllCookies();
     driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-    driver.navigate().to("https://www.saucedemo.com/");
-        // try {
+    driver.get("https://www.saucedemo.com/");
             WebElement title = driver.findElement(By.xpath("//div[contains(text(),'Swag Labs')]"));
               Assert.assertEquals(title.getText(), "Swag Labs");
               System.out.println("Title: "+title.getText());
-        // } catch (Exception e) {
-        //    System.out.println("shouldAnswerWithTrue Exception :  "+e.getMessage());
-        // }
       driver.close();
     }
 
