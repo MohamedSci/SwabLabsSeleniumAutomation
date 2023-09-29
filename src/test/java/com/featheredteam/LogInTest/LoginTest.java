af0f6d837acfd0d9e53142e5101fdbd711e2aa9f
MohamedSci <=>
2023-09-29 20:33:35 +0200

    more Scenarios LogIn 1

--- src/test/java/com/featheredteam/LogInTest/LoginTest.java
+++ src/test/java/com/featheredteam/LogInTest/LoginTest.java
@@ -22,7 +22,7 @@ public class LoginTest {
 
 
 
-        @Test
+        @Test(priority=1)
     public void ValidateWebsiteHeadingText(){
             WebElement title = driver.findElement(By.xpath("//div[contains(text(),'Swag Labs')]"));
             Assert.assertTrue(title.getText().equals("Swag Labs") );
@@ -30,8 +30,8 @@ public class LoginTest {
     }
     
     
-    @Test
-    public void AccessWithInValidUsername() throws InterruptedException{
+    @Test(priority = 2)
+    public void AccessWithStandard_user() throws InterruptedException{
 LoginPageLoad();
 
    System.out.println("Valid User name is ***************** "+ getAllUserList()[1]);
@@ -41,14 +41,36 @@ LoginPageLoad();
     getLogInBtn().click();
     
     Thread.sleep(3);
-    String followingPageUrl = driver.getCurrentUrl();
-       System.out.println("followingPageUrl ***************** "+ followingPageUrl);
-
-    Assert.assertTrue(followingPageUrl.contains("inventory"));
+    Assert.assertTrue(getCurrentPageUrl().contains("inventory"));
+           System.out.println("followingPageUrl ***************** "+ getCurrentPageUrl());
     }
 
-
     
+        @Test(priority = 3)
+    public void AccessWithLocked_out_user() throws InterruptedException{
+LoginPageLoad();
+       System.out.println("Valid User name is ***************** "+ getAllUserList()[2]);
+        getUserTextField().sendKeys(getAllUserList()[2]);
+             System.out.println("Password is ***************** "+ getValidPassword());
+    getPasswordTextField().sendKeys(getValidPassword());
+    getLogInBtn().click();
+    
+        Thread.sleep(3);
+       System.out.println("followingPageUrl ***************** "+ getCurrentPageUrl());
+
+     String errorMsg =  driver.findElement(By.xpath("//h3[@data-test='error']")).getText();
+       
+    Assert.assertTrue(getCurrentPageUrl().equals("https://www.saucedemo.com/"));
+    Assert.assertTrue(errorMsg.equals("Epic sadface: Sorry, this user has been locked out."));
+               System.out.println("errorMsg ***************** "+ errorMsg);
+    }
+@Test(dependsOnMethods={"AccessWithStandard_user"})
+    public void CanLogOut(){
+        menuDrawer().click();
+        logOutDrawer().click();
+            Assert.assertTrue(getCurrentPageUrl().equals("https://www.saucedemo.com/"));
+
+    }
     
 public void LoginPageLoad(){
        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
@@ -59,6 +81,9 @@ wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id=\"p
 wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id=\"login-button\"]")));
     }
 
+public String getCurrentPageUrl(){
+return driver.getCurrentUrl();
+}
 public  String[] getAllUserList(){
     String[] allUserList = driver.findElement(By.xpath("//h4[text()='Accepted usernames are:']/parent::div")).getText().split("\n");
 return allUserList;
@@ -78,7 +103,13 @@ return passwordList[1];
         public WebElement getLogInBtn() {
         return driver.findElement(By.xpath("//input[@id=\"login-button\"]"));
     }
-        
+     
+       public WebElement menuDrawer() {
+        return driver.findElement(By.xpath("//button[@id=\"react-burger-menu-btn\"]"));
+    }
+          public WebElement logOutDrawer() {
+        return driver.findElement(By.xpath("//a[@id=\"logout_sidebar_link\"]"));
+    }    
 
     
 
